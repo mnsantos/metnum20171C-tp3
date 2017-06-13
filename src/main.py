c5d9f6740df7496b2f3774aea7a52fef810f69e3
Martin Santos <martin.n.santos@gmail.com>
2017-06-13 00:09:51 -0300

    bla

--- src/main.py
+++ src/main.py
@@ -5,4 +5,4 @@ from analytics import *
 from graphics import *
 
 if __name__ == "__main__":
-	#graficar_lineas([1,2,3], [2,3,4], [1999,2000,2001], 'Cordoba')
+	graficar_lineas([1,2,3], [2,3,4], [1999,2000,2001], 'Cordoba')
